354ade3d5b40d904d4b4d64f469b1f7a385ca234
TimurMahmutovLOGITPV23 <tim.logitpv23@gmail.com>
2026-04-15 11:18:35 +0300

    Update CREATE, INSERT.sql

--- CREATE, INSERT.sql
+++ CREATE, INSERT.sql
@@ -26,3 +26,54 @@ VALUES('Boris', 'Sinine', '1999-5-5', 'Tallinn', 200, 1),
 --andmete uuendaamine tabelis
 UPDATE tootaja SET aadress='Tallinn', koormus=10, aktiivne=1
 WHERE tootajaID=1;
+
+--teine tabel
+CREATE TABLE toovahetus(
+toovahetusID int PRIMARY KEY identity(1,1),
+kuupäev date,
+tundideARV int,
+tootajaID int,
+FOREIGN KEY (tootajaID) REFERENCES tootaja(tootajaID))
+
+Select * from toovahetus
+select * from tootaja
+
+--täidame tabeli
+INSERT INTO toovahetus
+VALUES ('2026-04-14', 7, 3)
+
+
+CREATE TABLE koolitus(
+koolitusID int PRIMARY KEY identity(1,1), 
+nimetus varchar(60) not null, 
+kestvus int,
+algus date,
+lopp date,
+opetaja varchar(20))
+
+
+CREATE TABLE opetamine(
+opetamineID int PRIMARY KEY identity(1,1),
+tootajaID int,
+koolitusID int,
+tunnistus bit,
+hinne int,
+FOREIGN KEY (tootajaID) REFERENCES tootaja(tootajaID),
+FOREIGN KEY (koolitusID) REFERENCES koolitus(koolitusID))
+
+INSERT INTO koolitus
+VALUES
+('Matemaatika', 16, '2026-03-01', '2026-03-02', 'Arnold Saar'),
+('Inglise keel', 8, '2026-03-10', '2026-03-10', 'Tiina Lehtman'),
+('Linux Operatsioonisüsteem', 24, '2026-04-01', '2026-04-03', 'Jaan Tamm');
+
+INSERT INTO opetamine (tootajaID, koolitusID, tunnistus, hinne)
+VALUES
+(1, 1, 1, 5),
+(2, 2, 0, 4),
+(1, 3, 1, 5);
+
+SELECT * FROM koolitus
+SELECT * FROM opetamine
+SELECT * FROM toovahetus
+SELECT * FROM tootaja
